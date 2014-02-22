d6c890e807a17a095ab476aa454695683292c3b8
bizzbizz <bizz.pt@gmail.com>
2014-02-22 19:15:18 +0330

    some files added after npt all clear

---
+++ Soheil/Soheil.Core/ViewModels/PP/JobProductGroupVm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Soheil.Core.ViewModels.PP
+{
+	public class JobProductGroupVm : ProductGroupVm
+	{
+		public JobProductGroupVm(Model.ProductGroup model, DataServices.JobDataService jobDataService)
+		{
+			if (model == null) return;
+			_id = model.Id;
+			Name = model.Name;
+			Code = model.Code;
+
+			foreach (var p_model in model.Products)
+			{
+				var p = new JobProductVm(p_model, this, jobDataService);
+				Products.Add(p);
+			}
+
+		}
+	}
+}
---
+++ Soheil/Soheil.Core/ViewModels/PP/JobProductVm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Soheil.Core.ViewModels.PP
+{
+	public class JobProductVm : ProductVm
+	{
+		/// <summary>
+		/// can be used in JobEditor
+		/// </summary>
+		/// <param name="model"></param>
+		/// <param name="parentVm"></param>
+		public JobProductVm(Model.Product model, ProductGroupVm parentVm, DataServices.JobDataService jobDataService)
+			: base(model, parentVm)
+		{
+			CreateNewJob = new Commands.Command
+				(vm =>
+					{
+						var job = Soheil.Core.ViewModels.PP.Editor.PPEditorJob.CreateForProduct(model, jobDataService);
+						((Soheil.Core.ViewModels.PP.Editor.PPJobEditorVm)vm).JobList.Add(job);
+					}
+				);
+		}
+		//CreateNewJob Dependency Property
+		public Commands.Command CreateNewJob
+		{
+			get { return (Commands.Command)GetValue(CreateNewJobProperty); }
+			set { SetValue(CreateNewJobProperty, value); }
+		}
+		public static readonly DependencyProperty CreateNewJobProperty =
+			DependencyProperty.Register("CreateNewJob", typeof(Commands.Command), typeof(JobProductVm), new UIPropertyMetadata(null));
+	}
+}
